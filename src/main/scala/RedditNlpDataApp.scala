1866af59431cf90c7a9ceb8bba467bda89cab6c8
babyfacebrian <bkalx202@gmail.com>
2019-08-31 16:55:25 -0400

    Adding check for blank json data

--- src/main/scala/RedditNlpDataApp.scala
+++ src/main/scala/RedditNlpDataApp.scala
@@ -2,21 +2,21 @@
 object RedditNlpDataApp extends App {
 
   //  // Load Submissions
-    val submissionParams: Seq[(String, String)] = PreLoadDataUtils.getSubmissionParams
-    submissionParams.foreach(param => {
-      println(s"SUBMISSIONS STARTING -> TERM: ${param._1} : r/${param._2}")
-      loadSubmissions(param._1, param._2, fileType = "S")
-      println(s"SUBMISSIONS FINISHED -> TERM: ${param._1} : r/${param._2}")
-    })
+  val submissionParams: Seq[(String, String)] = PreLoadDataUtils.getSubmissionParams
+  submissionParams.foreach(param => {
+    println(s"SUBMISSIONS STARTING -> TERM: ${param._1} : r/${param._2}")
+    loadSubmissions(param._1, param._2, fileType = "S")
+    println(s"SUBMISSIONS FINISHED -> TERM: ${param._1} : r/${param._2}")
+  })
 
 
   // Load Comments
-    val commentsParams: Seq[(String, String)] = PreLoadDataUtils.getCommentsParams
-    commentsParams.foreach(param => {
-      println(s"COMMENTS STARTING -> TERM: ${param._1} : r/${param._2}")
-      loadComments(param._1, param._2, fileType = "C")
-      println(s"COMMENTS FINISHED -> TERM: ${param._1} : r/${param._2}")
-    })
+  val commentsParams: Seq[(String, String)] = PreLoadDataUtils.getCommentsParams
+  commentsParams.foreach(param => {
+    println(s"COMMENTS STARTING -> TERM: ${param._1} : r/${param._2}")
+    loadComments(param._1, param._2, fileType = "C")
+    println(s"COMMENTS FINISHED -> TERM: ${param._1} : r/${param._2}")
+  })
 
 
   private def loadComments(searchTerm: String, subreddit: String, fileType: String): Unit = {
